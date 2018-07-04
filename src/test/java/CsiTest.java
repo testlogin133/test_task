f75ba76560998dd4fd8941066d00b2940c884b34
iraili <bennylol34@gmail.com>
2018-07-04 22:00:05 +0300

    cucumberOptions fix

--- src/test/java/CsiTest.java
+++ src/test/java/CsiTest.java
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Configuration;
+import cucumber.api.CucumberOptions;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 import utils.PropertyManager;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features")
 public class CsiTest {
 
     @BeforeClass
